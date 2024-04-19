fb4546d037f089f5b4fa23e62a68f342596bdde3
zombkit <bakhmetstephen@gmail.com>
2024-04-18 21:24:07 -0700

    s

--- src/wasm_runner.rs
+++ src/wasm_runner.rs
@@ -12,13 +12,7 @@ pub fn native_setup<T: eframe::App + NewCC + 'static>(eapp: T) -> eframe::Result
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0])
-            .with_icon(
-                // NOTE: Adding an icon is optional
-                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
-                    .expect("Failed to load icon"),
-            ),
-        ..Default::default()
+            .with_min_inner_size([300.0, 220.0])..Default::default(),
     };
     eframe::run_native(
         "gold silver copper",
