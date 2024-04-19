78f1b8c7f4f2e92ec319f8800dcc6ad65bcf6300
zombkit <bakhmetstephen@gmail.com>
2024-04-18 21:25:35 -0700

    Update wasm_runner.rs

--- src/wasm_runner.rs
+++ src/wasm_runner.rs
@@ -12,7 +12,8 @@ pub fn native_setup<T: eframe::App + NewCC + 'static>(eapp: T) -> eframe::Result
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0])..Default::default(),
+            .with_min_inner_size([300.0, 220.0]),
+        ..Default::default()
     };
     eframe::run_native(
         "gold silver copper",
