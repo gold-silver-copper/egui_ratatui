429265213574e4d840586aef717a929e9dad0a3c
zombkit <bakhmetstephen@gmail.com>
2024-04-20 03:52:10 -0700

    more examples time

---
+++ examples/barchart_web/src/main.rs
@@ -0,0 +1,128 @@
+use ratatui::{
+    prelude::{Stylize, Terminal},
+    widgets::Paragraph,
+};
+use ratframe::RataguiBackend;
+
+use ratframe::NewCC;
+
+#[cfg(not(target_arch = "wasm32"))]
+use ratframe::native_setup;
+
+#[cfg(target_arch = "wasm32")]
+use ratframe::wasm_setup;
+
+// When compiling to web using trunk:
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    wasm_setup(HelloApp::default());
+}
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result<()> {
+    native_setup(HelloApp::default())
+}
+pub struct HelloApp {
+    terminal: Terminal<RataguiBackend>,
+}
+
+//l
+impl Default for HelloApp {
+    fn default() -> Self {
+        //Creating the Ratatui backend/ Egui widget here
+        let backend = RataguiBackend::new(100, 100);
+        let mut terminal = Terminal::new(backend).unwrap();
+        Self { terminal: terminal }
+    }
+}
+
+impl NewCC for HelloApp {
+    /// Called once before the first frame.
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        setup_custom_fonts(&cc.egui_ctx);
+        //Creating the Ratatui backend/ Egui widget here
+        let backend = RataguiBackend::new_with_fonts(
+            100,
+            100,
+            "Regular".into(),
+            "Bold".into(),
+            "Oblique".into(),
+            "BoldOblique".into(),
+        );
+        let mut terminal = Terminal::new(backend).unwrap();
+        Self { terminal: terminal }
+    }
+}
+
+impl eframe::App for HelloApp {
+    /// Called each time the UI needs repainting, which may be many times per second.
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        //call repaint here so that app runs continuously, remove if you dont need that
+        ctx.request_repaint();
+        self.terminal
+            .draw(|frame| {
+                let area = frame.size();
+                frame.render_widget(Paragraph::new("Hello Rataguiii").white().on_blue(), area);
+            })
+            .expect("epic fail");
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.add(self.terminal.backend_mut());
+
+            if ui.input(|i| i.key_released(egui::Key::Q)) {
+                panic!("HAVE A NICE WEEK");
+            }
+            if ui.input(|i| i.key_released(egui::Key::T)) {
+                ()
+            }
+            //KeyCode::Char(c) => app.on_key(c),
+        });
+    }
+}
+
+fn setup_custom_fonts(ctx: &egui::Context) {
+    // Start with the default fonts (we will be adding to them rather than replacing them).
+    let mut fonts = egui::FontDefinitions::default();
+
+    // Install my own font (maybe supporting non-latin characters).
+    // .ttf and .otf files supported.
+    fonts.font_data.insert(
+        "Regular".to_owned(),
+        egui::FontData::from_static(include_bytes!("../../../assets/fonts/Iosevka-Regular.ttf")),
+    );
+    fonts.families.insert(
+        egui::FontFamily::Name("Regular".into()),
+        vec!["Regular".to_owned()],
+    );
+    fonts.font_data.insert(
+        "Bold".to_owned(),
+        egui::FontData::from_static(include_bytes!("../../../assets/fonts/Iosevka-Bold.ttf")),
+    );
+    fonts.families.insert(
+        egui::FontFamily::Name("Bold".into()),
+        vec!["Bold".to_owned()],
+    );
+
+    fonts.font_data.insert(
+        "Oblique".to_owned(),
+        egui::FontData::from_static(include_bytes!("../../../assets/fonts/Iosevka-Oblique.ttf")),
+    );
+    fonts.families.insert(
+        egui::FontFamily::Name("Oblique".into()),
+        vec!["Oblique".to_owned()],
+    );
+
+    fonts.font_data.insert(
+        "BoldOblique".to_owned(),
+        egui::FontData::from_static(include_bytes!(
+            "../../../assets/fonts/Iosevka-BoldOblique.ttf"
+        )),
+    );
+    fonts.families.insert(
+        egui::FontFamily::Name("BoldOblique".into()),
+        vec!["BoldOblique".to_owned()],
+    );
+
+    // Tell egui to use these fonts:
+    ctx.set_fonts(fonts);
+}
